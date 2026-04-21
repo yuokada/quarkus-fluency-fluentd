03decaf623ce36aad58288f5b8a73b797a30d41b
Yukihiro Okada <yukihiro.okada@treasure-data.com>
2026-04-21 19:26:38 +0900

    Bump the fluent/fluentd to v1.19

--- integration-tests/src/test/java/io/github/yuokada/quarkus/extension/fluency/fluentd/it/FluentdTestResource.java
+++ integration-tests/src/test/java/io/github/yuokada/quarkus/extension/fluency/fluentd/it/FluentdTestResource.java
@@ -14,7 +14,7 @@ import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 public class FluentdTestResource implements QuarkusTestResourceLifecycleManager {
 
     private static final Logger log = Logger.getLogger(FluentdTestResource.class);
-    private static final String IMAGE = "fluent/fluentd:v1.18-1";
+    private static final String IMAGE = "fluent/fluentd:v1.19-2";
     private static final int FORWARD_PORT = 24224;
 
     private GenericContainer<?> fluentd;
